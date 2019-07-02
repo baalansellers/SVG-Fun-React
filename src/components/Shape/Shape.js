12f5288d2315eeb19673708c343965eeae9aa973
Alan Sellers <baalansellers@hotmail.com>
2019-07-02 18:03:38 -0400

    Messing around with an SVG path

--- src/components/Shape/Shape.js
+++ src/components/Shape/Shape.js
@@ -5,7 +5,10 @@ function Shape() {
     <div>
       <h2>Shape</h2>
       <svg height="100" width="300" viewBox="0, 0, 600, 100" style={{borderStyle: 'solid', borderWidth: '1px', borderColor: 'red'}}>
-          <circle fill="#00FF00" cx="150" cy="50" r="25"></circle>
+          <path d={`
+          M150 0
+          A 20 20 0 0 1 60 60
+          `}/>
       </svg>
     </div>
   );
